9c54f32442f3e51b1516d2d21669a26f6cd4c3c5
AnshuKumari <91437428+AnshuKumari999@users.noreply.github.com>
2023-03-15 15:53:27 +0530

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -1,12 +1,3 @@
-module "myvpc" {
-  source      = "../moduls/vpc"
-  vpc_cidr    = "10.0.0.0/16"
-  tenancy     = "default"
-  vpc_id      = module.myvpc.vpc_id
-  subnet_cidr = "10.0.1.0/24"
-}
-
-
 module "myec2" {
   source        = "../moduls/ec2"
   ami_id        = "ami-005f9685cb30f234b"
@@ -18,12 +9,6 @@ module "myec2" {
 
 }
 
-module "mygw" {
-    source = "../moduls/vpc"
-    vpc_id = module.myvpc.vpc_id
-    vpc_cidr = "10.0.0.0/16"
-  
-}
 
 module "aws_s3_bucket" {
   source = "../moduls/s3"
