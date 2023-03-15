881c76add8eb21092349b9dfe386fe4efb386dc8
AnshuKumari <91437428+AnshuKumari999@users.noreply.github.com>
2023-03-15 18:12:41 +0530

    Update main.tf

--- moduls/ec2/main.tf
+++ moduls/ec2/main.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "firstec2" {
-  #  for_each      = var.instance-map //toset(var.instance_name)
-   count = var.instance-name
+   for_each      = var.instance-name
+#    count = var.instance-name
    ami           = var.ami_id
   instance_type = "t2.micro"
   tags = {
-   Name  = count.index
+   Name  = each.value["name"]
    Owner = "anshu.kumari@cloudeq.com"
  }
  volume_tags = {
-  Name  = count.index
+  Name  = each.value["name"]
   Owner = "anshu.kumari@cloudeq.com"
  }
 
