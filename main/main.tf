91c844e6db75f22301479452bcaa343075503242
AnshuKumari <91437428+AnshuKumari999@users.noreply.github.com>
2023-03-15 22:21:02 +0530

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -1,21 +1,3 @@
-module "myec2" {
-  source        = "../moduls/ec2"
-  ami_id        = "ami-0d81306eddc614a45"
-#   for_each =    toset(var.instance-namex)
-  # instance-map = each.value
-#   instance-name = each.value
-  #instance_type = "t2.micro"
-  #subnet_id     = module.myvpc.subnet_id
-  instance-name = {
-    "a" = {
-      "name" = "abc"
-    },
-    "b" = {
-      "name" = "xyz"
-    }
-  }
-
-}
 
   module "myvpc" {
   source      = "../moduls/vpc"
